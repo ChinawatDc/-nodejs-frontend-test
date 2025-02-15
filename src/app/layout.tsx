1834ff45044bac791fc67eee664b612f4d6abaee
ChinawatDc <chinawat.dc@gmail.com>
2025-02-15 09:44:09 +0700

    fix: RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,28 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
+import { Prompt } from "next/font/google";
+import { useEffect } from "react";
 import "../styles/globals.css";
 import AppMenu from "./layout/menu";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const prompt = Prompt({
+  weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
+  display: "swap",
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Test Dev",
-  description: "Test Dev",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  useEffect(() => {
+    document.title = "Test Dev";
+  }, []);
+
+  //
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${prompt.className} antialiased`}>
         <AppMenu />
         {children}
       </body>
